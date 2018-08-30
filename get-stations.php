bf493f025bbf260299a51f6915e84cdc8ba49793
L-Eli <l.lin860218@gmail.com>
2018-08-30 20:08:00 +0800

    modified: format stations data

--- get-stations.php
+++ get-stations.php
@@ -9,10 +9,18 @@ $url = YOU_BIKE_URL;
 echo 'Getting data from \'http://data.ntpc.gov.tw/\' ...' . PHP_EOL . PHP_EOL;
 $content = file_get_contents($url);
 
+$data = json_decode($content, true);
+$result = [];
+foreach ($data as $station) {
+    unset($station['sbi']);
+    unset($station['bemp']);
+    $result[] = $station;
+}
+
 $file_path = '/data/stations.json';
 
 echo 'Writing data to \'' . $file_path . '\' ...' . PHP_EOL . PHP_EOL;
-file_put_contents(__DIR__ . $file_path, $content);
+file_put_contents(__DIR__ . $file_path, json_encode($result));
 
 echo 'Complete Successfully!' . PHP_EOL;
 exit();
