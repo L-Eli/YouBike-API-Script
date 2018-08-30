f4c26ca14d6ed885baf71c00f441787613f90854
L-Eli <l.lin860218@gmail.com>
2018-08-30 20:08:50 +0800

    modified: format status data

--- get-status.php
+++ get-status.php
@@ -11,10 +11,20 @@ $url = YOU_BIKE_URL;
 echo 'Getting data from \'http://data.ntpc.gov.tw/\' ...' . PHP_EOL . PHP_EOL;
 $content = file_get_contents($url);
 
+$data = json_decode($content, true);
+$result = [];
+foreach ($data as $status) {
+    $result[] = [
+        'sno' => $status['sno'],
+        'sbi' => $status['sbi'],
+        'bemp' => $status['bemp'],
+    ];
+}
+
 $file_path = '/data/status/' . date('YmdHis') . '.json';
 
 echo 'Writing data to \'' . $file_path . '\' ...' . PHP_EOL . PHP_EOL;
-file_put_contents(__DIR__ . $file_path, $content);
+file_put_contents(__DIR__ . $file_path, json_encode($result));
 
 echo 'Complete Successfully!' . PHP_EOL;
 exit();
